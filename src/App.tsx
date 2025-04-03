e085d08b6a952a1e85a30c1544f092d13633b018
Mwai Banda <bandamwai@gmail.com>
2025-04-02 20:28:19 -0500

    added the ability to to add all converted files to zip and download the zip

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,9 @@ import {
 } from "@/components/ui/card"
 import { Button } from './components/ui/button'
 import { useEffect, useState } from 'react'
-import { USFMParser, Filter, Validator } from 'usfm-grammar-web';
+import { USFMParser, Validator } from 'usfm-grammar-web';
 import { Skeleton } from './components/ui/skeleton';
+import { ZipArchive } from "@shortercode/webzip";
 
 type Processed = {
   original: string,
@@ -18,10 +19,7 @@ type Processed = {
   filename: string
 }
 
-async function getFileMedata(file: File) {
-  let text = await file.text()
-  return { name: file.name, text: text }
-} 
+
 
 function App() {
   const [files, setFiles] = useState<File[]>([])
@@ -38,39 +36,42 @@ function App() {
     initParser();
   }, []);
 
-  async function processFile(file: {name: string, text: string}) {
-    const usfmParser = new USFMParser(file.text);
-    const usxElem = usfmParser.toUSX() 
-    const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(usxElem as unknown as string, "application/xml");
-    const USX = new XMLSerializer().serializeToString(xmlDoc);
-    console.log(USX)
-    setProcessedFiles([...processedFiles, ...[{
-      original: file.name,
-      usx: USX,
-      filename: file.name.replace("usfm", "usx")
-    }]])  
+  async function createZip() {
+    const zip = new ZipArchive
+    for (const file of processedFiles) {
+        await zip.set(file.filename, file.usx)      
+    }
+    return zip
   }
 
-  useEffect(() => {
-    if (files.length > 0) {
-           
-      Promise.all(files.map(f => getFileMedata(f))).then(files => {
-        console.log(files)
-        Promise.all(files.filter(f => !processedFiles.find(p => p.original == f.name)).map(file  => {
-          processFile(file)
-        })).then(() => {
-          console.log("processing")
-        })
-      })
-    }
-  }, [files])
+  async function processeFiles() {
+    var mem = processedFiles
+    for (const file of files.filter(f => !processedFiles.find(p => p.original == f.name))) { 
+      const usfmParser = new USFMParser(await file.text());
+      const usxElem = usfmParser.toUSX() 
+      console.log(`coverted ${file.name}`)
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(usxElem as string, "application/xml");
+      console.log(`parsed ${file.name}`)
+      const USX = new XMLSerializer().serializeToString(xmlDoc);
+      console.log(`processed ${file.name}`)
+      mem = [...processedFiles, ...[{
+        original: file.name,
+        usx: USX,
+        filename: file.name.replace("usfm", "usx")
+      }]]       
+     }
+     console.log("mem", mem)
+     setProcessedFiles([...processedFiles, ...mem])
+  } 
+
+
   return (
     <div>
       <Card>
         <CardHeader className='flex flex-col'>
           <CardTitle>Convert USFM to USX</CardTitle>
-          <CardDescription>Card Description</CardDescription>
+          <CardDescription>Utility tool allow you select USFM files and convert them to USX</CardDescription>
         </CardHeader>
         <CardContent>
           <div className='min-w-[500px] flex flex-col justify-center items-center'>
@@ -79,7 +80,7 @@ function App() {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
                 </svg>
-                <span className='mt-2'>Click or drag your files here to convert</span>
+                <span className='mt-2'>Click to select your files here to convert</span>
               </span>
             </label>
             <input 
@@ -88,10 +89,32 @@ function App() {
               name='files' 
               style={{display: "none"}}
               multiple
-              onChange={e => {
+              onChange={async e => {
                 const fileList = e.target.files
-                if (fileList) setFiles(Array.from(fileList).filter(e => e.name.includes(".usfm")))
-                console.log(e.target.files)
+                if (fileList) {
+                  const recieved = Array.from(fileList).filter(e => e.name.includes(".usfm"))
+                  setFiles(recieved)
+                  console.log(recieved)
+                  var mem = processedFiles
+                  for (const file of recieved.filter(f => !processedFiles.find(p => p.original == f.name))) { 
+                    const USFM = await file.text()
+                    const usfmParser = new USFMParser(USFM);
+                    const usxElem = usfmParser.toUSX() 
+                    console.log(`coverted ${file.name}`)
+                    const parser = new DOMParser();
+                    const xmlDoc = parser.parseFromString(usxElem as string, "application/xml");
+                    console.log(`parsed ${file.name}`)
+                    const USX = new XMLSerializer().serializeToString(xmlDoc);
+                    console.log(`processed ${file.name}`)
+                    mem.push({
+                      original: file.name,
+                      usx: USX,
+                      filename: file.name.replace("usfm", "usx")
+                    })       
+                   }
+                   console.log("mem", mem)
+                   setProcessedFiles([...processedFiles, ...mem])
+                }
               }}
             ></input>
             <div className='min-w-[500px] flex flex-col justify-center items-start mt-5'>
@@ -101,7 +124,15 @@ function App() {
         </CardContent>
         <CardFooter className='flex flex-row w-full justify-between'>
         <p className='text-gray-500'>{files && `Total ${files.length} files`}</p>
-        <Button>
+        <Button onClick={() => {
+          createZip().then(zip => {
+            const element = document.createElement("a");
+            element.href = URL.createObjectURL(zip.to_blob());
+            element.download = "usx.zip";
+            document.body.appendChild(element); // Required for this to work in FireFox
+            element.click();
+          })
+        }}>
           <span className="flex items-center">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-4">
               <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
@@ -126,7 +157,7 @@ function FileList({ files, processed }: {files: File[] , processed: Processed[]}
 function ProcessedElement({ elm}: {elm: Processed | undefined}) {
   return <a 
     className='underline text-sky-600' 
-    href={URL.createObjectURL(new Blob([elm?.usx ?? ""], {type: "text/xml"}))}
+    href={elm ? URL.createObjectURL(new Blob([elm.usx ], {type: "text/xml"})) : undefined}
     download={elm?.filename}
   >{elm ? elm.filename : <Skeleton className="w-[200px] h-[20px] rounded-2" />}</a>
 }
